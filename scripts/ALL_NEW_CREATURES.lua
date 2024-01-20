f554248f44544eebc4dd5b03e78875d61d80dfc8
Victor Romanov <gerter666@hotmail.com>
2024-01-20 19:19:41 +0300

    start point

---
+++ scripts/ALL_NEW_CREATURES.lua
@@ -0,0 +1,11 @@
+GOG_NEW_CREATURES =
+{
+  [2100] = CREATURE_RECRUIT, -- зеленые ополченцы(3 грейд империи Единорога)
+  [2101] = CREATURE_BOWMAN, -- зеленые лучники Империи
+  [2102] = CREATURE_KNECHT, -- зеленые мечники империи
+  [2103] = CREATURE_SCOUT_GRIFFIN, -- зеленые грифоны империи
+  [2104] = CREATURE_CHAPLAIN, -- зеленые монахи империи
+  [2105] = CREATURE_KNIGHT, -- зеленые рыцари империи
+  [2106] = CREATURE_ELRATH_ANGEL, -- зеленые ангелы империи
+  
+}
